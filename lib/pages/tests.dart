9d64dba853cf70b5ed325bc3c7db2e289d120e8d
javioroz <javioroz@proton.me>
2022-08-04 01:06:54 +0200

    tests page finished

--- lib/pages/tests.dart
+++ lib/pages/tests.dart
@@ -18,56 +18,85 @@ class _TestsState extends ConsumerState<Tests> {
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(30.0),
-              child: Text('Click the tests in order.'),
-            ),
-            Center(
-              child: ethUtils.isLoading
-                  ? const CircularProgressIndicator()
-                  : Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 30),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              ethUtils.test1CreateStock();
-                            },
-                            child: const Text('Test 1: Create Stock'),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 50.0,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 30),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              ethUtils.test2SetCont();
-                            },
-                            child: const Text('Test 2: Set Cont Data'),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 50.0,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 30),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              ethUtils.test3Send();
-                            },
-                            child: const Text('Test 3: Send Conts'),
-                          ),
-                        ),
+        child: ethUtils.isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : ListView(
+                children: <Widget>[
+                  const ListTile(title: Text('Click the tests in order.')),
+                  const Divider(), //----------------------------------------------------
+                  ListTile(
+                    contentPadding:
+                        const EdgeInsets.symmetric(horizontal: 60.0),
+                    title: const Text('Test 1: Create Stock'),
+                    trailing: ElevatedButton(
+                        child: const Text('Test 1'),
+                        onPressed: () {
+                          ethUtils.test1CreateStock();
+                        }),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text("createStock(Secadoras_A, 200)"),
+                        Text("createStock(Lavadoras_B, 200)"),
+                        Text("createStock(Lavavajillas_A, 200);"),
                       ],
                     ),
-            ),
-          ],
-        ),
+                  ),
+                  const Divider(), //----------------------------------------------------
+                  ListTile(
+                    contentPadding:
+                        const EdgeInsets.symmetric(horizontal: 60.0),
+                    title: const Text('Test 2: Set Container Data'),
+                    trailing: ElevatedButton(
+                        child: const Text('Test 2'),
+                        onPressed: () {
+                          ethUtils.test2SetCont();
+                        }),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                            "setContData(1231, Secadoras_A, 50, Manufacturer, Warehouse_ES)"),
+                        Text(
+                            "setContData(1232, Lavadoras_B, 50, Manufacturer, Warehouse_FR)"),
+                        Text(
+                            "setContData(1233, Lavavajillas_A, 100, Manufacturer, Warehouse_FR)"),
+                        Text(
+                            "setContData(1234, Secadoras_A, 20, Warehouse_ES, Warehouse_FR)"),
+                        Text(
+                            "setContData(1235, Lavavajillas_A, 20, Warehouse_FR, Warehouse_DE)"),
+                        Text(
+                            "setContData(1236, Lavavajillas_A, 20, Warehouse_FR, Warehouse_ES)"),
+                        Text(
+                            "setContData(1237, Lavadoras_B, 30, Warehouse_FR, Warehouse_ES)"),
+                      ],
+                    ),
+                  ),
+                  const Divider(), //----------------------------------------------------
+                  ListTile(
+                    contentPadding:
+                        const EdgeInsets.symmetric(horizontal: 60.0),
+                    title: const Text('Test 3: Send Containers'),
+                    trailing: ElevatedButton(
+                        child: const Text('Test 3'),
+                        onPressed: () {
+                          ethUtils.test3Send();
+                        }),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text("sendContainer(1231) & receiveContainer(1231)"),
+                        Text("sendContainer(1232) & receiveContainer(1232)"),
+                        Text("sendContainer(1233) & receiveContainer(1233)"),
+                        Text("sendContainer(1234) & receiveContainer(1234)"),
+                        Text("sendContainer(1235) & receiveContainer(1235)"),
+                        Text("sendContainer(1236) & receiveContainer(1236)"),
+                        Text("sendContainer(1237) & receiveContainer(1237)"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
       ),
     );
   }
